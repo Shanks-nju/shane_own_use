0a8a1d32b1cad8c3b96de255f1e207e71b200665
shane <hxh0420@foxmail.com>
2018-08-12 01:57:02 +0800

    make the home page

--- src/main/java/cn/hxh/controller/Home.java
+++ src/main/java/cn/hxh/controller/Home.java
@@ -7,6 +7,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class Home {
     @GetMapping(value = "/")
     public String home() {
-        return "hello";
+        return html();
+    }
+
+    String[] hrefs = new String[]{"diary", "main", "resources", "swagger","anyone/owner.pdf"};
+    String[] texts = new String[]{"Diary", "Request Page", "Resources", "Swagger Page","Owner"};
+
+    String html() {
+        String top = "<!DOCTYPE html><html><head><title>home</title><meta charset=\"UTF-8\"><meta name=\"content-type\" content=\"text/html\"></head>";
+        String h1 = "<h1>Services List</h1><body><ul>";
+        String end = "</ul></body><html>";
+        return top + h1 + body() + end;
+    }
+
+    String body() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hrefs.length; i++) {
+            sb.append(href(hrefs[i], texts[i]));
+        }
+        return sb.toString();
+    }
+
+    String href(String path, String text) {
+        return "<li><a style=\"text-align: center\" href=\"/" + path + "\">" + text + "</a></li>";
     }
 }
